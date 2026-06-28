eca94dfa1104109b30399134a48ac81b478639a9
Thanh Loi <marcomoi395@gmail.com>
2026-06-28 08:14:24 +0700

    chore: default to light theme and hide theme selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,4 @@
 function onLoad() {
-  var themeSelector = document.getElementById('themeSelector');
-  for (var themeName in themeMap) {
-    var opt = document.createElement('option');
-    opt.value = themeName;
-    opt.innerHTML = capitalizeFirstLetter(themeName);
-    themeSelector.appendChild(opt);
-  }
-  const theme = localStorage.getItem('theme');
-  if (theme != null) {
-    themeSelector.value = theme;
-    toggleTheme();
-  }
-
   setupExternalLinks();
 }
 
@@ -45,22 +32,4 @@ function setupPageTransitions() {
       }, delay);
     });
   });
-}
-
-function toggleTheme() {
-  var themeSelector = document.getElementById('themeSelector');
-  var themeName = themeSelector.value;
-  localStorage.setItem('theme', themeName);
-  var element = document.documentElement;
-  changeTheme(element, themeMap[themeName]);
-}
-
-function changeTheme(element, theme) {
-  element.style.setProperty("--primary-background-color", theme['background-color']);
-  element.style.setProperty("--primary-text-color", theme['text-color']);
-  element.style.setProperty("--primary-highlight-color", theme['highlight-color']);
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
 }
